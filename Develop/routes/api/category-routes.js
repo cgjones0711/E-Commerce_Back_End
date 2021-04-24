5d571fd8019a80c38b833b19946751348d5f82fd
Clay Jones <cgjones0711@gmail.com>
2021-04-23 19:31:52 -0500

    setup complete not functioning

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  Category.findOne({ id: req.body.id}).then((categoryData) => {
+    res.json(categoryData);
+  });
+});
+
+router.post('/', (req, res) => {
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name,
+    
+  })
+    .then((newCategory) => {
+      
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  Category.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      id: req.body.id,
+      category_name: req.body.category_name,
+      
+    },
+    {
+      // Gets the books based on the isbn given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      // Sends the updated book as a json response
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
+});
+
+module.exports = router;
